4520fb182e5c71cee1df00dfc46857a3d6c2bd1f
Jordan Kisiel <jordan.kisiel@gmail.com>
2022-09-24 16:09:15 -0400

    Mobile Design finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,15 @@ module.exports = {
       //todo: add breakpoints
     },
     extend: {
+      fontFamily: {
+        'sans': ['Manrope', 'sans-serif']
+      },
       colors:{
-        
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)'
       }
     },
   },
