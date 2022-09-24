5c53536547b2bc2c5f157fe37c7dbf2c208ff915
Jordan Kisiel <jordan.kisiel@gmail.com>
2022-09-24 14:47:44 -0400

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      //todo: add breakpoints
+    },
+    extend: {
+      colors:{
+        
+      }
+    },
+  },
+  plugins: [],
+}
